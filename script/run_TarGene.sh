fb2b8908e046f6855d078b70a71c06a6d43e91e3
Kenneth Shang <Kenneth.Shang@foxmail.com>
2019-05-29 13:10:34 +0800

    Update run_TarGene.sh

--- script/run_TarGene.sh
+++ script/run_TarGene.sh
@@ -16,14 +16,14 @@ if [ -f "recruit.fa" ]; then
    echo "File recruit.fa exists, SKIPPING TarGene (manually delete if you want to rerun)"
 else
    echo "### Build bloom filter"
-   mkdir $INDEX_DIR || { echo "mkdir ${INDEX_DIR} failed"; exit 1;}
+   mkdir $REF_DIR || { echo "mkdir ${REF_DIR} failed"; exit 1;}
    if [$RRP_FLAG == 1]; then
-      bowtie2-build -f /mnt/gs18/scratch/users/yannisun/AMY/TarGene/test_data/recruit_ref.fa ${INDEX_DIR}/IDX || { echo "Bowtie2-build failed"; exit 1;}
+      bowtie2-build -f ${WORKDIR}/recruit_ref.fa ${REF_DIR}/IDX || { echo "Bowtie2-build failed"; exit 1;}
    else
-      bowtie2-build -f $REF_POS ${INDEX_DIR}/IDX || { echo "Bowtie2-build failed"; exit 1;}
+      bowtie2-build -f $REF_POS ${REF_DIR}/IDX || { echo "Bowtie2-build failed"; exit 1;}
    fi
    
-   bowtie2 -x ${INDEX_DIR}/IDX -f $FASTA_FILE -p $THREAD_NUM -S $SAM_FILE || { echo "Bowtie2 failed"; exit 1;}
+   bowtie2 -x ${REF_DIR}/IDX -f $FASTA_FILE -p $THREAD_NUM -S $SAM_FILE || { echo "Bowtie2 failed"; exit 1;}
    samtools view -F 4 -h $SAM_FILE >mapped.sam || { echo "Samtools failed"; exit 1;}
    mkdir $INDEX_DIR || { echo "mkdir $INDEX_DIR failed"; exit 1;}
    $BASEDIR/build -f $FASTA_FILE -o ${INDEX_DIR}/IDX || { echo "build function failed"; exit 1;}
