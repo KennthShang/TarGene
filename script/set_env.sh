eb8d11be7504bf15fa9332d6779b8d6e50f1c766
Kenneth Shang <Kenneth.Shang@foxmail.com>
2019-05-29 15:49:02 +0800

    Update set_env.sh

--- script/set_env.sh
+++ script/set_env.sh
@@ -2,12 +2,13 @@
 
 #### start of configuration
 
-####### Adjust values for these parameters ########
-#       
-#       
-#       
-#       
-###################################################
+########## Adjust values for these parameters ###########
+#  FASTA_FILE_POS, BASEDIR, WORKDIR, REFERENCE_DATABASE
+#  REF_POS, THREAD_NUM, RRP_FLAG, STRIDE, LENGTH
+#  K, OVERLAP, K_MERS, MAXIMUN_MEM
+#  Note: k and THREAD_NUM should follow: k = n*THREAD_NUM
+#        n = 1, 2, 3,...,N
+#########################################################
 
 ## THIS SECTION MUST BE MODIFIED FOR YOUR FILE SYSTEM. MUST BE ABSOLUTE PATH
 FASTA_FILE_POS=/mnt/gs18/scratch/users/yannisun/AMY/TarGene/test_data/reads.fa
@@ -25,7 +26,7 @@ THREAD_NUM=8 # number of threads you want to use
 RRP_FLAG=false  # Whether you want to run RRP or not(1 for YES, 0 for NO)
 NUM_RECRUIT=200  # Number of threads that you want to use
 STRIDE=1  # Strides for creating reads from the dataset
-LENGTH=100
+LENGTH=100 # Length for creating reads from the dataset
 
 ## TarGene Parameters
 SAM_FILE=result.sam # Name of the sam file (output of bowtie2)
@@ -40,6 +41,6 @@ K_MERS=11,17,33   # K-mers number
 MAXIMUN_MEM=200  # Maximum memory will be used in SPAdes 
 OUTPUT_FOLDER=SPAdes.output
 
-
+NAME=TarGene_${OVERLAP}
 
 #### end of configuration
