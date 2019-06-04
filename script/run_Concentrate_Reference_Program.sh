2ffcf2d1e96319a39f81479e84b03be2a373422f
Kenneth Shang <Kenneth.Shang@foxmail.com>
2019-06-04 15:47:38 +0800

    Update and rename run_Recruit_Reference_Program.sh to run_Concentrate_Reference_Program.sh

--- script/run_Concentrate_Reference_Program.sh
+++ script/run_Concentrate_Reference_Program.sh
@@ -14,14 +14,14 @@ mkdir -p ${WORKDIR}/${NAME}/RRP || { echo "mkdir -p ${WORKDIR}/RRP failed"; exit
 cd ${WORKDIR}/${NAME}/RRP
 
 ## build bloom filter
-if [ -f "recruit_ref.fa" ]; then
+if [ -f "concentrate_ref.fa" ]; then
    echo "File recruit_ref.fa exists, SKIPPING RRP (manually delete if you want to rerun)" 
 else
    python3 ${BASEDIR}/tools/recruit_ref.py -n $NUM_RECRUIT -s $STRIDE -l $LENGTH -f $REFERENCE_DATABASE -t $THREAD_NUM || { echo "Recruit Reference Program failed"; exit 1;}
    if [ -f "data_remain.fa" ]; then
-      cat data_remain.fa recruited_reference/* > recruit_ref.fa 
+      cat data_remain.fa concentrate_reference/* > concentrate_ref.fa 
    else
-      cat recruited_reference/* > recruit_ref.fa 
+      cat concentrate_reference/* > concentrate_ref.fa 
    fi
-   echo "Recruit Reference Program complete!"
+   echo "Concentrate Reference Program complete!"
 fi
